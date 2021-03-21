e80082f7b814b31b6fb4713555d2229dbeadf830
Namazbek Bekaryssuly <NBekaryssuly@beeline.kz>
2021-03-22 01:35:21 +0600

    migration seeding functionality added

--- cmd/sales-admin/main.go
+++ cmd/sales-admin/main.go
@@ -1,44 +1,88 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	_ "github.com/lib/pq"
+	"github.com/nomikz/training/internal/platform/conf"
 	"github.com/nomikz/training/internal/platform/database"
 	"github.com/nomikz/training/internal/schema"
+	"github.com/pkg/errors"
 	"log"
+	"os"
 )
 
 func main() {
+	if err := run(); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	// =========================================================================
+	// Get Configuration
+
+	var cfg struct {
+		DB struct {
+			User       string `conf:"default:postgres"`
+			Password   string `conf:"default:postgres,noprint"`
+			Host       string `conf:"default:localhost:5433"`
+			Name       string `conf:"default:postgres"`
+			DisableTLS bool   `conf:"default:true"`
+		}
+		Args conf.Args
+	}
+
+	if err := conf.Parse(os.Args[1:], "sales", &cfg); err != nil {
+		if err == conf.ErrHelpWanted {
+			usage, err := conf.Usage("SALES", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config usage")
+			}
+			fmt.Println(usage)
+			return nil
+		}
+		return errors.Wrap(err, "parsing config")
+	}
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return errors.Wrap(err, "generating config for output")
+	}
+	log.Printf("main : Config :\n%v\n", out)
 
 	// =========================================================================
 	// Set up dependencies
 
-	db, err := database.Open()
+	db, err := database.Open(database.Config{
+		Host:       cfg.DB.Host,
+		User:       cfg.DB.User,
+		Password:   cfg.DB.Password,
+		Name:       cfg.DB.Name,
+		DisableTLS: cfg.DB.DisableTLS,
+	})
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer db.Close()
 	if err := db.Ping(); err != nil {
 		log.Println(err)
 	}
 
-
-	flag.Parse()
-
-	switch flag.Arg(0) {
+	switch cfg.Args.Num(0) {
 	case "migrate":
 		if err := schema.Migrate(db); err != nil {
 			log.Fatal("applying migrations error: ", err)
 		}
 		log.Println("Migrations completed")
-		return
+		return nil
 	case "seed":
 		if err := schema.Seed(db); err != nil {
-			log.Fatal("applying seeding error: ", err)
+			return errors.Wrap(err, "applying seeding")
 		}
 		log.Println("Seeding  completed")
-		return
+		return nil
 	}
+
+	return nil
 }
-
-
